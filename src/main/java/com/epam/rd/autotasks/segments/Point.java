bf5a492dcdaa41e3465694a377ecbc0b0939286b
Gagik Kazandjyan <bi0820a@outlook.com>
2023-05-28 20:14:45 +0400

    Solved at 28/05/2023

--- src/main/java/com/epam/rd/autotasks/segments/Point.java
+++ src/main/java/com/epam/rd/autotasks/segments/Point.java
@@ -1,8 +1,10 @@
 package com.epam.rd.autotasks.segments;
 
+import java.util.Objects;
+
 class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public Point(final double x, final double y) {
         this.x = x;
@@ -15,5 +17,28 @@ class Point {
 
     public double getY() {
         return y;
+    }
+
+    public double distanceTo(Point other) {
+        double dx = other.x - x;
+        double dy = other.y - y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Point other = (Point) obj;
+        return Double.compare(other.x, x) == 0 && Double.compare(other.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -1,25 +1,67 @@
 package com.epam.rd.autotasks.segments;
 
-import static java.lang.Math.abs;
-import static java.lang.Math.sqrt;
-import static java.lang.StrictMath.pow;
-
-class Segment {
+public class Segment {
+    private final Point start;
+    private final Point end;
 
     public Segment(Point start, Point end) {
-
+        if (start == null || end == null) {
+            throw new IllegalArgumentException("Segment points cannot be null");
+        }
+        if (start.equals(end)) {
+            throw new IllegalArgumentException("Degenerate segment: start and end points are the same");
+        }
+        this.start = start;
+        this.end = end;
     }
 
-    double length() {
-
+    public double length() {
+        return start.distanceTo(end);
     }
 
-    Point middle() {
-
+    public Point middle() {
+        double x = (start.getX() + end.getX()) / 2;
+        double y = (start.getY() + end.getY()) / 2;
+        return new Point(x, y);
     }
 
-    Point intersection(Segment another) {
+    public Point intersection(Segment another) {
+        double x1 = start.getX();
+        double y1 = start.getY();
+        double x2 = end.getX();
+        double y2 = end.getY();
+        double x3 = another.start.getX();
+        double y3 = another.start.getY();
+        double x4 = another.end.getX();
+        double y4 = another.end.getY();
 
+        double denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+        if (denominator == 0) {
+            // Segments are collinear
+            return null;
+        }
+
+        double intersectX = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denominator;
+        double intersectY = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denominator;
+
+        Point intersection = new Point(intersectX, intersectY);
+
+        // Check if the intersection point is within both segments
+        if (isPointOnSegment(intersection) && another.isPointOnSegment(intersection)) {
+            return intersection;
+        } else {
+            return null;
+        }
+    }
+
+    private boolean isPointOnSegment(Point point) {
+        double minX = Math.min(start.getX(), end.getX());
+        double maxX = Math.max(start.getX(), end.getX());
+        double minY = Math.min(start.getY(), end.getY());
+        double maxY = Math.max(start.getY(), end.getY());
+
+        return point.getX() >= minX && point.getX() <= maxX && point.getY() >= minY && point.getY() <= maxY;
     }
 
 }
